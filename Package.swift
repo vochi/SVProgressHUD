6e5fa6ad431efc8245b36f79e8c1ade1c6af9cc4
Vladimir Kashkevich <vkashkevich@pinterest.com>
2022-07-18 17:14:20 +0200

    Added swift package manager

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "SVProgressHUD",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "SVProgressHUD",
+            targets: ["SVProgressHUD"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "SVProgressHUD",
+            dependencies: [],
+            path: "SVProgressHUD",
+            resources: [.copy("Resources/SVProgressHUD.bundle")])
+    ]
+)
